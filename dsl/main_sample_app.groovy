13cd9dfd70cb89f50645efc5545d5ac73630558e
MaK <mkmarek91@gmail.com>
2020-09-26 15:27:37 +0200

    Adding dsl + cleaning unused files

---
+++ dsl/main_sample_app.groovy
@@ -0,0 +1,31 @@
+multibranchPipelineJob("target-app") {
+  branchSources {
+    github {
+      apiUri('https://api.github.com')
+      buildForkPRHead(false)
+      buildForkPRMerge(false)
+      buildOriginBranch(true)
+      buildOriginBranchWithPR(true)
+      buildOriginPRHead(false)
+      buildOriginPRMerge(true)
+      checkoutCredentialsId('github-user')
+      scanCredentialsId('github-user')
+      repoOwner('makmk')
+      repository('sample-app')
+      id('target-app-branch')
+    }
+  }
+  factory {
+    workflowBranchProjectFactory {
+      scriptPath('Jenkinsfile')
+    }
+  }
+  orphanedItemStrategy {
+    defaultOrphanedItemStrategy {
+      pruneDeadBranches(true)
+      daysToKeepStr("5")
+      numToKeepStr("20")
+      
+    }
+  }
+}
